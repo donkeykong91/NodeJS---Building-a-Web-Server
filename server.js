6bec60d5ccfa6c7c583ca4feb15ee070c2158dd0
donkeykong91 <darylnator@gmail.com>
2018-07-03 04:02:36 -0700

    Changed content type to html

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require("http");
 
 var server = http.createServer(function (req, res) {
 
-  res.writeHead(200, {"Content-Type": "text/plain"});
+  res.writeHead(200, {"Content-Type": "text/html"});
 
   res.end(`
       <!DOCTYPE html>
